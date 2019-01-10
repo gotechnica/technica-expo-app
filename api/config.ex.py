4a573405feef5e583f58926970d150aa69ce9867
Timothy Chen <timothy.chen01@gmail.com>
2019-01-10 15:45:14 -0500

    Make comments more generic

--- api/config.ex.py
+++ api/config.ex.py
@@ -1,6 +1,6 @@
 # Config file. Rename as config.py and fill in keys/variables
 MONGO_DBNAME = 'your-db-name-here'                                        # e.g. 'expo-app'
-MONGO_HOST = 'uri-to-connect-to-your-db-including-username-and-password'  # e.g. 'mongodb://expo-app-user:an38N4o*121L4@ds261460.mlab.com:61460/expo-app' (if using mLab)
+MONGO_HOST = 'uri-to-connect-to-your-db-including-username-and-password'  # e.g. 'mongodb://<dbuser>:<dbpassword>@ds261460.mlab.com:61460/expo-app' (if using mLab)
 ADMIN_ACCESS_CODE = 'super-secret-admin-access-code-here'                 # Used to log into Godmode dashboard
 SECRET_KEY = 'some-super-secret-string-used-for-flask-sessions'           # A long string of random characters
 
