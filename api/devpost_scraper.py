ad615b8509a49740332991118fa55f3c5467fb24
naveenr414 <dsfan414@gmail.com>
2019-03-04 18:19:56 -0500

    Updated how devpost scraper works

--- api/devpost_scraper.py
+++ api/devpost_scraper.py
@@ -1,50 +1,25 @@
 from bs4 import BeautifulSoup
 import urllib.request as ur
-from functools import reduce 
+from functools import reduce
+import re
 
 def get_challenges(link):
     sock = ur.urlopen(link)
     soup = BeautifulSoup(sock,"html.parser")
 
-    #Find the prizes
-    filters = soup.find("form",{"class":"filter-submissions"}).findAll("ul")
-    prize_list = filters[0].findAll("label")
-    prize_ids = [prize.find("input")["value"] for prize in prize_list]
-    prize_list = [row.contents[-1] for row in prize_list]
+    prizes = soup.find("article",{"id":"prizes"}).find("ul").findAll("li")[1:]
+    prizes = list(map(lambda x: x.find("h6").text.strip(),prizes))
+    prizes = [i for i in prizes if "-" in i]
 
-    if(link[-1]!="/"):
-        link+="/"
+    for i in range(len(prizes)):
+        description = prizes[i]
+        print(description)
+        name = "-".join(description.split("-")[:-1]).strip()
+        company = description.split("\n")[0].split("-")[-1].strip()
+        num = 1
+        if re.search("\(\d+\)",description):
+            num = int(description.split("(")[-1].strip(")"))
+        prizes[i] = (name,company,num)
 
-    num_winners = []
-
-    #Find the number of winners for each prize
-    for id_ in prize_ids:
-        winners = 0
-        new_link = link+"search?&prize_filter%5Bprizes%5D%5B%5D="+str(id_)
-
-        id_soup = BeautifulSoup(ur.urlopen(new_link),"html.parser")
-
-        #Find the number of pages of projects
-        num_pages = id_soup.find("ul",{"class":"pagination"})
-        if(num_pages):
-            num_pages = int(num_pages.findAll("li")[-2].text)
-        else:
-            num_pages = 1
-
-        # Add to gether winners on each page
-        for i in range(1,num_pages+1):
-            new_link = link+"search?page="+str(i)+"&prize_filter%5Bprizes%5D%5B%5D="+str(id_)
-
-            id_soup = BeautifulSoup(ur.urlopen(new_link),"html.parser")
-            winners+=len(id_soup.findAll("img",{"class":"winner"}))
-
-        num_winners.append(winners)
-
-    #Split it up into challenge, company and winners 
-    ret_list = []
-
-    for i,row in enumerate(prize_list):
-        if(len(row.split(" - ")) == 2):
-            ret_list.append(tuple(row.split(" - "))+(num_winners[i],))
+    return prizes
     
-    return ret_list
