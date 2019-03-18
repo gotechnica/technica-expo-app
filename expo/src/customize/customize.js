59914733fffa3a5ea59712b756a7a74f2fd47e00
Timothy Chen <timothy.chen01@gmail.com>
2019-03-18 03:56:36 -0400

    [Part 3] Bitcamp customization
    - Update color scheme
    - Tweak y-axis spacing of logo header

--- expo/src/customize/customize.js
+++ expo/src/customize/customize.js
@@ -1,5 +1,5 @@
 export default {
-    "table_color": ["#FF7BAC", "#B6A1C7", "#17E3E3"],
-    "toggle_button": ["#2f2f2f"],
+    "table_color": ["#FF8860", "#A58D7C", "#FF5B60"],
+    "toggle_button": ["#1A2E33"],
     "hackathon_name": "Bitcamp"
 };
