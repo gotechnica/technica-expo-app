ed939dc1b265b84bc9d0587c7ebc921a738fa83f
Andrew Mao <pazzun@gmail.com>
2019-08-11 18:55:02 -0400

    Deleted stub, replaced with yaml file

--- openapi-server-stub/swagger_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.challenge import Challenge
-from swagger_server.models.inline_response200 import InlineResponse200
-from swagger_server.models.project import Project
