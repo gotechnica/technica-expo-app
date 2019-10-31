f55ac5426bdfaf26d0e7b2610dc9735a004cbb76
Evan McIntire <mcintire.evan@gmail.com>
2019-10-31 15:21:41 -0400

    Add server stuff

--- api/config.ex.py
+++ api/config.ex.py
@@ -4,5 +4,7 @@ MONGO_HOST = 'uri-to-connect-to-your-db-including-username-and-password'  # e.g.
 ADMIN_ACCESS_CODE = 'super-secret-admin-access-code-here'                 # Used to log into Godmode dashboard
 SECRET_KEY = 'some-super-secret-string-used-for-flask-sessions'           # A long string of random characters
 
+EXPO_LENGTH = 60 # Total length of the expo (in minutes). Used for computing time per table for sponsors.
+
 # If you aren't allowing this option for hackers, replace the string with the python None variable
 CUSTOM_DEVPOST_STAY_AT_TABLE_QUESTION = 'Exact string of the question configured in Devpost for checking which table a hack is currently at (if it must stay at that current table for expo).'
