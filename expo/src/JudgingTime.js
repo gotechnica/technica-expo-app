48e5c3f4a8fa07f1c4fa0608cf79a3bde744f7be
Evan McIntire <mcintire.evan@gmail.com>
2019-11-05 15:29:42 -0500

    Add extra time at end

--- expo/src/JudgingTime.js
+++ expo/src/JudgingTime.js
@@ -9,17 +9,22 @@ import React, {useState} from "react";
 
 export default function JudgingTimes(props) {
     const [judges, setJudges] = useState(1);
+    const [endTime, setEndTime] = useState(10);
 
     if (props.count === 0) {
         return "You have no projects to judge."
     }
 
     const plural = judges === 1 ? "judge" : "judges";
-    const time = (props.time / props.count) * judges;
+    const time = ((props.time - endTime) / props.count) * judges;
 
     return (
     <div className="card">
         You have {props.count} projects to judge in {props.time} minutes. <br/>
-        <div className="nowrap">Given <input className="inline-numinput" type="number" id="numJudges" name="judges" min="1" value={judges} onChange={(e) => {setJudges(parseInt(e.target.value))}} /> {plural}, each judge can spend about {time.toFixed(1)} minutes per hack.</div>
+        <div className="nowrap">
+            Given
+                <input className="inline-numinput" type="number" id="numJudges" name="judges" min="1" value={judges} onChange={(e) => {setJudges(parseInt(e.target.value))}} /> {plural}, and
+                <input className="inline-numinput" type="number" id="endTime" name="selectionTime" min="1" value={endTime} onChange={(e) => {setEndTime(parseInt(e.target.value))}} /> minutes at the end to select a winner, <br/>
+            each judge can spend about {time.toFixed(1)} minutes per hack.</div>
     </div>);
 }
