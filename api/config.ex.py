30636fff3a724ebb5be4b654c685bd9624c1ae5f
Chris Sullivan <codemastermd@gmail.com>
2020-02-17 17:44:45 -0500

    Adding mongodb local development config example url

--- api/config.ex.py
+++ api/config.ex.py
@@ -1,6 +1,6 @@
 # Config file. Rename as config.py and fill in keys/variables
 MONGO_DBNAME = 'your-db-name-here'                                        # e.g. 'expo-app'
-MONGO_HOST = 'uri-to-connect-to-your-db-including-username-and-password'  # e.g. 'mongodb://<dbuser>:<dbpassword>@ds261460.mlab.com:61460/expo-app' (if using mLab)
+MONGO_HOST = 'uri-to-connect-to-your-db-including-username-and-password'  # e.g. 'mongodb://<dbuser>:<dbpassword>@ds261460.mlab.com:61460/expo-app' (if using mLab) or 'mongodb://admin:password@db:27017/expo-testing?authSource=admin' for local development
 ADMIN_ACCESS_CODE = 'super-secret-admin-access-code-here'                 # Used to log into Godmode dashboard
 SECRET_KEY = 'some-super-secret-string-used-for-flask-sessions'           # A long string of random characters
 
