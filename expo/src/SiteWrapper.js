8d5d3ebe7135b0683c5409b8e9d237366a9e40bc
Eric Chen <63428949+cloudui@users.noreply.github.com>
2023-03-31 13:21:24 -0400

    Retheme Site. (#67)

--- expo/src/SiteWrapper.js
+++ expo/src/SiteWrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import hackathon_logo from "customize/imgs/hackathon-logo.svg";
+import hackathon_logo from "customize/imgs/bitcamp-logo.png";
 import "SiteWrapper.css";
 
 /* Header / constant app features */
@@ -9,7 +9,9 @@ let SiteWrapper = (content) => (
   <div className="App">
     <nav className="navbar navbar-expand-md">
       <Link to="/">
-        <img className="logo" src={hackathon_logo} alt="Logo" />
+        <div class="header">
+          <img className="logo" src={hackathon_logo} alt="Logo" />
+        </div>
       </Link>
     </nav>
     <div className="container">{content}</div>
